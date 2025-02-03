555a47544bf6598f1e1331c1e0b1ed8a204d47ee
Yazeed-Alharbi <yazeedm255@gmail.com>
2025-02-03 20:42:32 +0300

    Refactor SpacingStyles to use MediaQueryData for responsive padding

--- lib/common/styles/spcaing_styles.dart
+++ lib/common/styles/spcaing_styles.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class SpacingStyles {
   final BuildContext context;
   late final EdgeInsets defaultPadding;
+  late final MediaQueryData data;
 
   SpacingStyles(this.context) {
+    data = MediaQuery.of(context);
     defaultPadding = EdgeInsets.fromLTRB(
-        MediaQuery.of(context).size.height * 0.03,
-        MediaQuery.of(context).size.height * 0.1,
-        MediaQuery.of(context).size.height * 0.03,
+        data.orientation == Orientation.portrait ? data.size.width * 0.05 : data.size.width * 0.1 ,
+        data.orientation == Orientation.portrait ? data.size.height * 0.1 : 0 ,
+        data.orientation == Orientation.portrait ? data.size.width * 0.05 : data.size.width * 0.1 ,
         0);
   }
 }
