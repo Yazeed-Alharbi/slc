f37abb7618e60cbe88e9d2ffc5a2a330f64d3440
Yazeed-Alharbi <yazeedm255@gmail.com>
2025-02-06 13:17:26 +0300

    Refactor authentication methods to use SLCFlushbar for error handling

--- lib/firebaseUtil/auth_services.dart
+++ lib/firebaseUtil/auth_services.dart
@@ -1,27 +1,47 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
+import 'package:slc/common/widgets/slcflushbar.dart';
 
 class AuthenticationService {
-  Future<void> signup({required String email, required String password}) async {
+  Future<bool> signup({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
     try {
       await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
+      return true;
     } on FirebaseAuthException catch (e) {
       String message = "";
       if (e.code == 'weak-password') {
-        message = 'the password provided is too weak';
+        message = 'The password provided is too weak';
       } else if (e.code == 'email-already-in-use') {
         message = 'An account with this email exists';
       }
-      Fluttertoast.showToast(
-          msg: message,
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.SNACKBAR);
-    } catch (e) {}
+
+      SLCFlushbar.show(
+        context: context,
+        message: message,
+        type: FlushbarType.error,
+      );
+
+      return false;
+    } catch (e) {
+      SLCFlushbar.show(
+        context: context,
+        message: "An unexpected error occurred.",
+        type: FlushbarType.error,
+      );
+      return false;
+    }
   }
 
-  Future<void> signin({required String email, required String password}) async{
+  Future<void> signin({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
     try {
       await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
@@ -30,10 +50,17 @@ class AuthenticationService {
       if (e.code == 'user-not-found' || e.code == 'wrong-password') {
         message = 'Entered credentials are incorrect!';
       }
-      Fluttertoast.showToast(
-          msg: message,
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.SNACKBAR);
-    } catch (e) {}
+      SLCFlushbar.show(
+        context: context,
+        message: message,
+        type: FlushbarType.error,
+      );
+    } catch (e) {
+      SLCFlushbar.show(
+        context: context,
+        message: "An unexpected error occurred.",
+        type: FlushbarType.error,
+      );
+    }
   }
 }
