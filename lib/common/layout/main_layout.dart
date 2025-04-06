ac066780de7dbd08c2db2597182a2c111a0501d8
Yazeed-Alharbi <yazeedm255@gmail.com>
2025-04-06 23:35:43 +0300

    Add initialIndex parameter to MainLayout and persist selected index across rebuilds

--- lib/common/layout/main_layout.dart
+++ lib/common/layout/main_layout.dart
@@ -4,21 +4,40 @@ import 'package:slc/features/calendar/screens/calendar.dart';
 import 'package:slc/features/course%20management/screens/courses.dart';
 import 'package:slc/features/home/screens/home.dart';
 import 'package:slc/models/Student.dart';
-import 'package:slc/main.dart'; // Import to access the global homeScreenKey
+import 'package:slc/main.dart';
 
 class MainLayout extends StatefulWidget {
   final Student student;
-  const MainLayout({Key? key, required this.student}) : super(key: key);
+  final int initialIndex;
+
+  const MainLayout({
+    Key? key,
+    required this.student,
+    this.initialIndex = 0,
+  }) : super(key: key);
+
   @override
   _MainLayoutState createState() => _MainLayoutState();
 }
 
 class _MainLayoutState extends State<MainLayout> {
-  int _selectedIndex = 0;
+  // Static variable to persist the selected index across rebuilds
+  static int _persistedIndex = 0;
+  late int _selectedIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    // Use the persisted index or the provided initialIndex
+    _selectedIndex =
+        _persistedIndex != 0 ? _persistedIndex : widget.initialIndex;
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      // Update the persisted index when user changes tabs
+      _persistedIndex = index;
     });
   }
 
